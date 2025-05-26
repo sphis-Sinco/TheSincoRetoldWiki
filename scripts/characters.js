bd0287f7b9449eb14cdf578f841a7fb4c8cdcb95
Sinco <sinconsistencia@gmail.com>
2025-05-26 09:47:20 -0500

    Update characters.js

--- scripts/characters.js
+++ scripts/characters.js
@@ -5,65 +5,67 @@ const characters = [
   {
     name: "Sinco",
     description: "Teenager speedster protecting Tempo City by any means necessary.",
-    powerLevel: null,
-    image: "content/characters/sinco.jpg",
+    powerLevel: 1080,
+    image: "content/characters/sinco.png",
     firstAppearance: 1,
-    lastAppearance: 10,
-    birthday: "", // add in YYYY-MM-DD format if known
+    lastAppearance: 85,
+    birthday: "2011-09-19",
     funFacts: [
-      "Fastest in Tempo City.",
-      "Never gives up."
+      "Based off his creator heavily",
+      "Died twice, both times getting some kind of special training"
     ]
   },
   {
     name: "TJ",
     description: "Best-friend of Sinco, another teenager speedster, protecting Boredom City instead.",
     powerLevel: null,
-    image: "content/characters/tj.jpg",
+    image: "content/characters/tj.png",
     firstAppearance: 1,
-    lastAppearance: 9,
+    lastAppearance: 30,
     birthday: "",
     funFacts: []
   },
   {
     name: "Tirok",
     description: "Anti-Hero engineer scientist focused on his own beliefs.",
-    powerLevel: null,
-    image: "content/characters/tirok.jpg",
-    firstAppearance: 2,
-    lastAppearance: 11,
-    birthday: "",
-    funFacts: ["Created Sinco’s clone."]
+    powerLevel: 1,
+    image: "content/characters/tirok.png",
+    firstAppearance: 1,
+    lastAppearance: 85,
+    birthday: "2000-04-05",
+    funFacts: ["Wishes for his own power"]
   },
   {
     name: "Osin",
     description: "Clone of Sinco made by Tirok, treats Tirok like a father figure.",
-    powerLevel: null,
-    image: "content/characters/osin.jpg",
-    firstAppearance: 3,
-    lastAppearance: 11,
-    birthday: "",
+    powerLevel: 1020,
+    image: "content/characters/osin.png",
+    firstAppearance: 1,
+    lastAppearance: 85,
+    birthday: "2023-10-08",
     funFacts: []
   },
   {
     name: "Docaci",
     description: "Mother of Sinco, speedster who got her powers disabled by Tirok.",
     powerLevel: null,
-    image: "content/characters/docaci.jpg",
-    firstAppearance: 1,
-    lastAppearance: 8,
-    birthday: "",
+    image: "content/characters/docaci.png",
+    firstAppearance: 24,
+    lastAppearance: 55,
+    birthday: "1993-03-14",
     funFacts: []
   },
   {
     name: "Karo",
     description: "Sinco's grandfather, speedster, the first person to turn super on Earth, and the reason Docaci and Sinco have speedster powers now.",
     powerLevel: null,
-    image: "content/characters/karo.jpg",
-    firstAppearance: 0,
-    lastAppearance: 5,
-    birthday: "",
-    funFacts: []
+    image: "content/characters/karo.png",
+    firstAppearance: 47,
+    lastAppearance: 61,
+    birthday: "1974-06-23",
+    funFacts: [
+      "Made for a prequel book that tells the story of the first person to go super"
+    ]
   }
 ];
 
@@ -85,7 +87,6 @@ function renderCharacterCard(character) {
       ${character.funFacts && character.funFacts.length ? `<li>${strong("Fun Facts:")}<ul>${character.funFacts.map(fact => `<li>${fact}</li>`).join('')}</ul></li>` : ''}
     </ul>
   `;
-
   return createCardSection(character.name, content);
 }
 
