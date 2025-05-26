7e6d45e2f6de86f08dc8735ff2c5dd6d0c6b4bb4
Sinco <sinconsistencia@gmail.com>
2025-05-26 12:30:46 -0500

    Create characters.js

---
+++ scripts/characters.js
@@ -0,0 +1,9 @@
+const characters = [
+  { name: "Sinco", description: "" },
+  { name: "TJ", description: "" },
+  { name: "Tirok", description: "" },
+  { name: "Osin", description: "" },
+  { name: "Crepode", description: "" },
+  { name: "Docaci", description: "" },
+  { name: "Karo", description: "" }
+];
